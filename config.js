f103b094c862d405658e16e0a4953019ab8c6917
AqibSher <sheraqib492@gmail.com>
2024-10-07 10:02:50 -0700

    Prepare for Heroku deployment

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-export const SERVER_IP = 'http://192.168.1.5:3000'; // Replace this with your actual IP
+export const SERVER_IP = 'http://192.168.10.179:3000'; // Replace this with your actual IP
